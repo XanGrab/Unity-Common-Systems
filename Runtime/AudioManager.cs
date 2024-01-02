2afc4ede04b7fd2381648e869ccf69651fed6a05
XanGrab <60899653+XanGrab@users.noreply.github.com>
2024-01-01 22:26:44 -0600

    Namespace * Cleaning up code with regions * Renamed some fields

--- Runtime/AudioManager.cs
+++ Runtime/AudioManager.cs
@@ -3,194 +3,189 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
 
-/// <summary>
-/// Class <c>AudioManager</c> is used to trigger sounds and play music from scenes in Unity. Sounds should be manually added to the sounds array in the Inspector for use at Runtime
-/// </summary>
-[RequireComponent(typeof(AudioSource))]
-public class AudioManager : MonoBehaviour {
-    // Singleton
-    private static AudioManager _instance;
-    public static AudioManager Instance { get { return _instance; } }    
-
-    private static AudioSource speaker;
-
-    [SerializeField] private Sound[] persistSounds;
-    [SerializeField] private bool playOnAwake = false;
-
-    // Sounds that should remain persistent across the runtime of the game. These will be loaded in Awake effectivley similar to preloadAudioData
-
-    // Manager internal array of all playable sounds
-    private static Sound[] sounds;
-
-    void Awake() {
-        if(_instance != null && _instance != this) {
-            Destroy(gameObject);
-            return;
-        }else{
-            _instance = this;
-        }
-
-        speaker = gameObject.GetComponent<AudioSource>();
-
-        sounds = new Sound[persistSounds.Length];
-        foreach (Sound sound in persistSounds) sound.LoadClips();
-        Array.Copy(persistSounds, sounds, persistSounds.Length);
-
-        if(playOnAwake && sounds.Length > 0) Play(sounds[0].name);
-    }
-
-    /// <param name="name">the name of the desired Sound</param>
-    /// <returns>specified Sound from array if found; null otherwise</returns>
-    private static Sound GetSound(string name) {
-        return Array.Find(sounds, sound => sound.name == name); 
-    }
-
-    /// <summary> Mutator Method <c>ReadyAudio</c> prepares the primary AudioSource to be played in the current scene with the specified Sound</summary>
-    /// <param name="name">the name of the Sound to be played</param>
-    /// <param name="clip">the index of the audio clip to played in the Sound <paramref name="name"/></param>
-    private static AudioSource ReadySource(string name, AudioSource src = null){
-        Sound sound = GetSound(name);
-        if(sound == null){
-            Debug.LogError("[AudioManager > ReadySource] Sound '" + name + "' not found.");
-            return null;
-        }        
-
-        if(!src) src = speaker; 
-
-        src.clip = sound.getClip();
-        src.volume = sound.Volume;
-        src.pitch = sound.Pitch;
-        src.loop = sound.Loop;
-        src.outputAudioMixerGroup = sound.OutputMixerGroup;
-        return src;
-    }
-
-    public static void LoadSounds(Sound[] toLoad) {
-        foreach (Sound sound in toLoad){
-            sound.LoadClips();
-        }
-
-        int prevLength = sounds.Length;
-        Array.Resize(ref sounds, sounds.Length + toLoad.Length);
-        Array.Copy(toLoad, 0, sounds, prevLength, toLoad.Length);
-    }
-
+namespace SoundSystem {
     /// <summary>
-    /// Get the current timestamp from an AudioSource
-    public static float GetTimestamp() { return speaker.time; }
+    /// Class <c>AudioManager</c> is used to trigger sounds and play music from scenes in Unity. Sounds should be manually added to the sounds array in the Inspector for use at Runtime
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioManager : MonoBehaviour {
+        // Singleton
+        private static AudioManager _instance;
+        public static AudioManager Instance { get { return _instance; } }    
 
-    /// <summary>
-    /// Method <c>PlayOnce</c> triggers a specified sound to be played in the current scene
-    /// <param name="name">the name of the Sound in manager's sounds to play</param>
-    public static void PlayOnce(string name) {
-        AudioSource src = ReadySource(name);
-        if(!src) { 
-            return;
-        } else {
-            speaker = src;
-        }
-        speaker.PlayOneShot(speaker.clip);
-            Debug.Log("DEBUG [AudioManager > PlayOnce] " + name);
-    }
+        private static AudioSource speaker;
 
-    /// <summary> Method <c>Play</c> is used to Play a specified sound in the current scene, if it is not already playing </summary>
-    /// <param name="name">the name of the Sound in manager's sounds to play</param>
-    /// <param name="time">playback position in seconds</param>
-    public static void Play(string name, float time = 0f) {
-        AudioSource src = ReadySource(name);
-        if(!src) { 
-            return;
-        } else {
-            speaker = src;
-        }
+        [SerializeField] private Sound[] persistSounds;
+        [SerializeField] private bool playOnAwake = false;
 
-        speaker.time = time;
-        if(!speaker.isPlaying) {
-            Debug.Log("DEBUG [AudioManager > Play] " + name);
-            speaker.Play();
-        }
-    }
+        // Sounds that should remain persistent across the runtime of the game. These will be loaded in Awake effectivley similar to preloadAudioData
 
-    /// <summary> Method <c>Play</c> is used to stop a specified sound in the current scene </summary>
-    public static void PauseToggle(AudioSource src = null) {
-        if(!src) src = speaker; 
+        // Manager internal array of all playable sounds
+        private static Sound[] tracks;
 
-        if(src.isPlaying){
-            src.Pause();
-        }else{
-            src.UnPause();
-        }
-    }
+        void Awake() {
+            if(_instance != null && _instance != this) {
+                Destroy(this);
+                return;
+            }else{
+                _instance = this;
+            }
 
-    /// <summary> Method <c>Play</c> is used to stop a specified sound in the current scene </summary>
-    public static void Stop(AudioSource src = null) {
-        if(!src) src = speaker; 
-        src.Stop();
-    }
-    
-    /// <summary> Method <c>FadeOut</c> is used to graduatley stop an audio clip</summary>
-    /// <param name="step">the step by which the sound shound fade per the <paramref name="rate"/> which defaults to 1/10th of a second.<param>
-    /// <param name="rate">the rate at which the volume of the playing clip should be diminished per <paramref name="step"/></param>
-    IEnumerator FadeOut(AudioSource src = null, float step = 0.1f, float rate = 0.1f) {
-        if (step <= 0) {
-            Debug.LogError("ERROR [AudioManager > FadeOut] Invalid param step and rate must be > 0");
-            yield break;
-        }
-        float clipVolume = speaker.volume;
+            speaker = gameObject.GetComponent<AudioSource>();
 
-        for (float vol = clipVolume; vol > 0; vol -= rate){
-            speaker.volume = vol;
-            yield return new WaitForSeconds(step);
-        }
-    }
+            tracks = new Sound[persistSounds.Length];
+            foreach (Sound sound in persistSounds) sound.LoadClips();
+            Array.Copy(persistSounds, tracks, persistSounds.Length);
 
-    /// <summary> Method <c>FadeIn</c> is used to graduatley start an audio clip</summary>
-    /// <param name="name">the name of the Sound to fade in<param>
-    /// <param name="step">the step by which the sound shound fade per the <paramref name="rate"/> which defaults to 1/10th of a second.<param>
-    /// <param name="rate">the rate at which the volume of the playing clip should be diminished per <paramref name="step"/></param>
-    IEnumerator FadeIn(string name, AudioSource src = null, float step = 0.1f, float rate = 0.1f) {
-        if (step <= 0 || rate <= 0.1f) {
-            Debug.LogError("ERROR [AudioManager > FadeOut] Invalid param step and rate must be > 0");
-            yield break;
-        }
-        speaker = ReadySource(name);
-        AudioSource source = ReadySource(name);
-        if(!source) { 
-            yield break;
-        } else {
-            speaker = src;
+            if(playOnAwake && tracks.Length > 0) Play(tracks[0].name);
         }
 
-        float clipVolume = speaker.volume;
+        #region HelperMethods
 
-        for (float vol = 0.0f; vol < clipVolume; vol += rate){
-            speaker.volume = vol;
-            yield return new WaitForSeconds(step);
-        }
-    }
-
-    /// <summary> Method <c>FadeIn</c> is used to graduatley transition from one audio clip to another</summary>
-    /// <param name="name">the name of the Sound to cross fade to<param>
-    /// <param name="step">the step by which the sound shound fade per the <paramref name="rate"/> which defaults to 1/10th of a second.<param>
-    /// <param name="rate">the rate at which the volume of the playing clip should be diminished per <paramref name="step"/></param>
-    IEnumerator CrossFade(string name, float step = 0.1f, float rate = 0.1f) {
-        if (step <= 0 || rate <= 0.1f) {
-            Debug.LogError("ERROR [AudioManager > FadeOut] Invalid param step and rate must be > 0");
-            yield break;
-        }
-        AudioSource newSource = gameObject.AddComponent<AudioSource>();
-        newSource = ReadySource(name, newSource);
-        newSource.time = speaker.time;
-        newSource.volume = 0f;
-        float clipVolume = speaker.volume;
-
-        for (float vol = clipVolume; vol > 0; vol -= rate){
-            speaker.volume = vol;
-            newSource.volume += rate;
-            yield return new WaitForSeconds(step);
+        private static Sound GetSound(string name) {
+            return Array.Find(tracks, sound => sound.name == name); 
         }
 
-        Destroy(speaker);
-        speaker = newSource;
+        private static AudioSource ReadySource(Sound sound, AudioSource src = null){
+            if(!src) src = speaker; 
+
+            src.clip = sound.getClip();
+            src.volume = sound.Volume;
+            src.pitch = sound.Pitch;
+            src.loop = sound.Loop;
+            src.outputAudioMixerGroup = sound.Mixer;
+            return src;
+        }
+
+        /// <summary>
+        /// Get the current timestamp from an AudioSource
+        public static float GetTimestamp() { return speaker.time; }
+
+        public static void LoadSounds(Sound[] toLoad) {
+            foreach (Sound sound in toLoad){
+                sound.LoadClips();
+            }
+
+            int prevLength = tracks.Length;
+            Array.Resize(ref tracks, tracks.Length + toLoad.Length);
+            Array.Copy(toLoad, 0, tracks, prevLength, toLoad.Length);
+        }
+
+        #endregion
+
+        public static void PlayOnce(string name) {
+            Sound sound = GetSound(name);
+            AudioSource src = ReadySource(sound);
+            if(!src) { 
+                return;
+            } else {
+                speaker = src;
+            }
+            speaker.PlayOneShot(speaker.clip);
+                Debug.Log("DEBUG [AudioManager > PlayOnce] " + name);
+        }
+
+        public static void Play(string name, float fadeTime = 0f, float time = 0f) {
+            Sound sound = GetSound(name);
+            AudioSource src = ReadySource(sound);
+            if(!src) { 
+                return;
+            } else {
+                speaker = src;
+            }
+
+            speaker.time = time;
+            if(!speaker.isPlaying) {
+                speaker.Play();
+                Debug.Log("DEBUG [AudioManager > Play] " + name);
+            }
+        }
+
+        public static void Play(Sound sound, float fadeTime = 0f, float time = 0f) {
+            AudioSource src = ReadySource(sound);
+            if(!src) { 
+                return;
+            } else {
+                speaker = src;
+            }
+
+            speaker.time = time;
+            if(!speaker.isPlaying) {
+                speaker.Play();
+                Debug.Log("DEBUG [AudioManager > Play] " + sound.name);
+            }
+        }
+
+        public static void PauseToggle(AudioSource src = null) {
+            if(!src) src = speaker; 
+
+            if(src.isPlaying){
+                src.Pause();
+            }else{
+                src.UnPause();
+            }
+        }
+
+        public static void Stop(AudioSource src = null) {
+            if(!src) src = speaker; 
+            src.Stop();
+        }
+        
+        IEnumerator FadeOut(AudioSource src = null, float step = 0.1f, float rate = 0.1f) {
+            if (step <= 0) {
+                Debug.LogError("ERROR [AudioManager > FadeOut] Invalid param step and rate must be > 0");
+                yield break;
+            }
+            float clipVolume = speaker.volume;
+
+            for (float vol = clipVolume; vol > 0; vol -= rate){
+                speaker.volume = vol;
+                yield return new WaitForSeconds(step);
+            }
+        }
+
+        IEnumerator FadeIn(string name, AudioSource src = null, float step = 0.1f, float rate = 0.1f) {
+            if (step <= 0 || rate <= 0.1f) {
+                Debug.LogError("ERROR [AudioManager > FadeOut] Invalid param step and rate must be > 0");
+                yield break;
+            }
+            Sound sound = GetSound(name);
+            speaker = ReadySource(sound);
+            AudioSource source = ReadySource(sound);
+            if(!source) { 
+                yield break;
+            } else {
+                speaker = src;
+            }
+
+            float clipVolume = speaker.volume;
+
+            for (float vol = 0.0f; vol < clipVolume; vol += rate){
+                speaker.volume = vol;
+                yield return new WaitForSeconds(step);
+            }
+        }
+
+        IEnumerator CrossFade(string name, float step = 0.1f, float rate = 0.1f) {
+            if (step <= 0 || rate <= 0.1f) {
+                Debug.LogError("ERROR [AudioManager > FadeOut] Invalid param step and rate must be > 0");
+                yield break;
+            }
+            AudioSource newSource = gameObject.AddComponent<AudioSource>();
+            Sound sound = GetSound(name);
+            newSource = ReadySource(sound, newSource);
+            newSource.time = speaker.time;
+            newSource.volume = 0f;
+            float clipVolume = speaker.volume;
+
+            for (float vol = clipVolume; vol > 0; vol -= rate){
+                speaker.volume = vol;
+                newSource.volume += rate;
+                yield return new WaitForSeconds(step);
+            }
+
+            Destroy(speaker);
+            speaker = newSource;
+        }
     }
 }
--- Runtime/Sound.cs
+++ Runtime/Sound.cs
@@ -1,67 +1,88 @@
-using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Audio;
 
-/// <summary>
-/// Unity Sctiptable Object <c>Sound</c> represents an audio clip to be played in game
-/// </summary>
-[CreateAssetMenu(menuName = "Sound")]
-public class Sound : ScriptableObject {
-    [SerializeField] private AudioClip[] clips;
+namespace SoundSystem {
 
-    [SerializeField] private ClipOrder clipOrder = ClipOrder.ordered; 
-
-    [SerializeField] private AudioMixerGroup outputGroup;
-    public AudioMixerGroup OutputMixerGroup => outputGroup;
-
-    private int clipIndex = -1;
-
-    [Range(0f, 1f)]
-    [SerializeField] private float volume = 0f;
-    public float Volume => volume;
-
-    [Range(-10, 10)]
-    [SerializeField] private int pitch = 0;
-    public int Pitch => pitch;
-
-    [SerializeField] private bool loop = false;
-    public bool Loop => loop;
-
-    public void LoadClips(){
-        foreach(AudioClip clip in clips) {
-            clip.LoadAudioData();
-        }
+    enum ClipType {
+        manual = 0,
+        ordered = 1,
+        random = 2,
+        additive = 3,
     }
 
-    public void setClipIndex(int index){
-        if (clipOrder != ClipOrder.manual) {
-            Debug.LogWarning("Warning [Sound " + name + "] should not call setClipIndex unless ClipOrder is set to manual");
+    /// <summary>
+    /// Unity Sctiptable Object <c>Sound</c> represents an audio clip to be played in game
+    /// </summary>
+    [CreateAssetMenu(menuName = "Sound")]
+    public class Sound : ScriptableObject {
+        [SerializeField] private AudioClip[] clips;
+
+        [SerializeField] private ClipType clipType = ClipType.ordered; 
+
+        [SerializeField] private AudioMixerGroup _mixer;
+        public AudioMixerGroup Mixer => _mixer;
+
+        private int clipIndex = 0;
+
+        [Range(0f, 1f)]
+        [SerializeField] private float volume = 0f;
+        public float Volume => volume;
+
+        [Range(-10, 10)]
+        [SerializeField] private int pitch = 0;
+        public int Pitch => pitch;
+
+        [SerializeField] private bool loop = false;
+        public bool Loop => loop;
+
+        #region ManageClips
+
+        public void LoadClips(){
+            foreach(AudioClip clip in clips) {
+                clip.LoadAudioData();
+            }
         }
 
-        clipIndex = index;
-    }
+        public void setClipIndex(int index){
+            if (clipType != ClipType.manual) {
+                Debug.LogWarning("Warning [Sound " + name + "] should not call setClipIndex unless ClipOrder is set to manual");
+            }
 
-    public AudioClip getClip() {
-        switch(clipOrder) {
-            case ClipOrder.ordered:
-                clipIndex = (clipIndex + 1) % clips.Length;
-                break;
-            case ClipOrder.random:
-                clipIndex = Random.Range(0, clips.Length);
-                break;
-            default:
-                break;
+            clipIndex = index;
         }
-        return clips[clipIndex];
-    }
 
-    public AudioClip getClip(int index) { return clips[index]; }
+        public AudioClip getClip() {
+            switch(clipType) {
+                case ClipType.ordered:
+                    clipIndex = (clipIndex + 1) % clips.Length;
+                    break;
+                case ClipType.random:
+                    clipIndex = Random.Range(0, clips.Length);
+                    break;
+                default:
+                    break;
+            }
+            return clips[clipIndex];
+        }
+
+        public AudioClip getClip(int index) { return clips[index]; }
+
+        #endregion //ManageClips
+
+        #region AudioCtrl
+
+        public void Play(float fadeTime, float time) {
+            AudioManager.Play(this, fadeTime, time);
+        }
+
+        public void PauseToggle() {
+            AudioManager.PauseToggle();
+        }
+
+        public void Stop() {
+            AudioManager.Stop();
+        }
+        #endregion //AudioCtrl
+    }
 }
-
-enum ClipOrder {
-    ordered = 0,
-    random = 1,
-    manual = 2,
-}
-
